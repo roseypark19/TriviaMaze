a1336226c0fda0879ae259302dc95c6b18831c79
Parker Rosengreen <parker19@uw.edu>
2021-05-31 13:16:49 -0700

    added documentation for components package

--- src/components/TriviaComponent.java
+++ src/components/TriviaComponent.java
@@ -1,11 +1,30 @@
+/*
+ * TriviaComponent.java
+ * TCSS360 - Trivia Maze
+ * Spring 2021
+ */
+
 package components;
 
 import java.io.Serializable;
 
 import view.PlayPanel.TriviaPanel;
 
+/**
+ * TriviaComponent is an interface implemented by all play panel components which supply
+ * responses to displayed trivia questions.
+ * 
+ * @author Parker Rosengreen, Rebekah Parkhurst, Artem Potafiy
+ * @version 31 May 2021
+ */
 public interface TriviaComponent extends Serializable {
 	
+	/**
+	 * Adds an action listener to this component which sends the choice value of this
+	 * component to the provided parent trivia panel for processing. 
+	 * 
+	 * @param theTrivPan the parent trivia panel of this component
+	 */
 	void addActionListener(final TriviaPanel theTrivPan);
 
 }
--- src/components/TrueFalseButton.java
+++ src/components/TrueFalseButton.java
@@ -1,3 +1,9 @@
+/*
+ * TrueFalseButton.java
+ * TCSS360 - Trivia Maze
+ * Spring 2021
+ */
+
 package components;
 
 import java.awt.Color;
@@ -10,18 +16,40 @@ import javax.swing.JButton;
 
 import view.PlayPanel.TriviaPanel;
 
+/**
+ * TrueFalseButton is a class for entering answers to true/false trivia questions
+ * 
+ * @author Parker Rosengreen, Rebekah Parkhurst, Artem Potafiy
+ * @version 31 May 2021
+ */
 public class TrueFalseButton extends JButton implements TriviaComponent {
 	
-	/**
-	 * 
-	 */
+	/** The serial version UID */
 	private static final long serialVersionUID = 731396828680626895L;
+	
+	/** The button width */
 	private static final int WIDTH = 120;
+	
+	/** The button height */
 	private static final int HEIGHT = 55;
+	
+	/** The button font */
 	private static final Font FONT = new Font(Font.MONOSPACED, Font.BOLD, 35);
+	
+	/** The button border width */
 	private static final int BORDER_WIDTH = 3;
+	
+	/** The button's choice boolean */
 	private final boolean myBool;
 	
+	/**
+	 * Constructs a new TrueFalseButton with a provided choice boolean and parent
+	 * trivia panel.
+	 * 
+	 * @param theChoice the choice boolean
+	 * @param theTrivPan the parent trivia panel
+	 * @throws NullPointerException if theTrivPan is null
+	 */
 	public TrueFalseButton(final boolean theBoolean, final TriviaPanel theTrivPan) {
 		super(String.valueOf(theBoolean));
 		Objects.requireNonNull(theTrivPan, "Trivia panels must be non-null!");
@@ -35,7 +63,15 @@ public class TrueFalseButton extends JButton implements TriviaComponent {
 		addActionListener(theTrivPan);
 	}
 	
+	/**
+	 * Adds an action listener to this button which sends the choice boolean of this
+	 * button to the provided parent trivia panel for processing.
+	 * 
+	 * @param theTrivPan the parent trivia panel of this button
+	 * @throws NullPointerException if theTrivPan is null
+	 */
 	public void addActionListener(final TriviaPanel theTrivPan) {
+		Objects.requireNonNull(theTrivPan, "Trivia panels must be non-null!");
 		super.addActionListener(theEvent -> theTrivPan.processResponse(String.valueOf(myBool)));
 	}
 
