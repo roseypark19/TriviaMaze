3c807bedfb3922fad8940d39f9adf29ee6e19376
Parker Rosengreen <parker19@uw.edu>
2021-05-31 21:24:29 -0700

    added documentation to program package

--- src/program/GameLauncher.java
+++ src/program/GameLauncher.java
@@ -1,13 +1,31 @@
+/*
+ * GameLauncher.java
+ * TCSS360 - Trivia Maze
+ * Spring 2021
+ */
+
 package program;
 
 import view.GameFrame;
 
+/**
+ * GameLauncher is a class which launches the Maze Hops game.
+ * 
+ * @author Parker Rosengreen, Rebekah Parkhurst, Artem Potafiy
+ * @version 31 May 2021
+ */
 public class GameLauncher {
 	
-	public GameLauncher() {
+	/** Displays a new game frame which hosts the Maze Hops game. */
+	private GameLauncher() {
 		new GameFrame();
 	}
 	
+	/**
+	 * Instantiates a new game launcher which displays the Maze Hops game to the screen.
+	 * 
+	 * @param theArgs command line arguments - ignored by this program
+	 */
 	public static void main(final String[] theArgs) {
 		new GameLauncher();
 	}
