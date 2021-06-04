8bbe6395b110fe2d920039ca65ee2bacdb6db4e4
Parker Rosengreen <parker19@uw.edu>
2021-06-03 20:37:37 -0700

    removed unused imports and switched to junit 5 instead of 4

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -6,12 +6,10 @@
 
 package view;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FileDialog;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
